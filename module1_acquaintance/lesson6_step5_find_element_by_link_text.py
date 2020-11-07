936e9d0d56d400e41051f2ee03cb42a0628a428c
Delictum <thedrakoneragoni@gmail.com>
2020-11-07 20:36:51 +0300

    Added lesson6_step5_find_element_by_link_text.py - completed task

---
+++ module1_acquaintance/lesson6_step5_find_element_by_link_text.py
@@ -0,0 +1,37 @@
+"""
+The task: https://stepik.org/lesson/138920/step/5?unit=196194
+"""
+
+from selenium import webdriver
+import time
+import math
+
+
+link_tested_site = "http://suninjuly.github.io/find_link_text"
+
+try:
+    browser = webdriver.Chrome()
+    browser.get(link_tested_site)
+
+    cipher_link = str(math.ceil(math.pow(math.pi, math.e)*10000))
+    found_link = browser.find_element_by_link_text(cipher_link)
+    time.sleep(1)
+    found_link.click()
+
+    input1 = browser.find_element_by_tag_name("input")
+    input1.send_keys("Ivan")
+
+    input2 = browser.find_element_by_name("last_name")
+    input2.send_keys("Petrov")
+
+    input3 = browser.find_element_by_class_name("city")
+    input3.send_keys("Smolensk")
+
+    input4 = browser.find_element_by_id("country")
+    input4.send_keys("Russia")
+
+    button = browser.find_element_by_css_selector("button.btn")
+    button.click()
+finally:
+    time.sleep(20)
+    browser.quit()
